96d357b16f536cfa8a77dfa3878c344bad51d25f
Michaela <kolesar.michaela@gmail.com>
2024-02-07 11:03:10 +0100

    profile database

---
+++ database/migrations/2024_02_07_093050_add_personal_info_to_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->binary('profile_picture')->default(null)->nullable();
+            $table->string('username')->default(null)->nullable();
+            $table->string('location')->default(null)->nullable();
+            $table->string('phone_number')->default(null)->nullable();
+            $table->string('street')->default(null)->nullable();
+            $table->string('house_number')->default(null)->nullable();
+            $table->integer('postcode')->default(null)->nullable();
+            $table->string('city')->default(null)->nullable();
+            $table->date('birthday')->default(null)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
